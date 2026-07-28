d5fb88fe4ad0cf8cc9a38c38e6994514af957020
Bikee Prajapati <bikeeprajapati1@gmail.com>
2026-07-28 19:08:48 +0545

    feat(server): add middleware for body parsing, cookies, and CORS

--- server/app.ts
+++ server/app.ts
@@ -1,3 +1,25 @@
-import express from 'express';
-
+require('dotenv').config();
+import express, {NextFunction,Request, Response} from 'express';
 export const app = express();
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+//body parser middleware
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true }));
+//cookie parser middleware
+app.use(cookieParser());
+app.use(cors({
+    origin: process.env.origin?.split(','),
+    credentials: true,
+}));
+
+//testing api
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World');
+});
+
+//unknown route middleware
+app.use((req: Request, res: Response, next: NextFunction) => {
+    res.status(404).json({ message: 'Route not found' });
+});
