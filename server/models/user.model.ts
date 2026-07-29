66fd6717928c51d4258c4bce84a607c8452c99e4
Bikee Prajapati <bikeeprajapati1@gmail.com>
2026-07-29 17:22:50 +0545

    feat(models): add user model with schema and password hashing

---
+++ server/models/user.model.ts
@@ -0,0 +1,91 @@
+import mongoose, { Document, Schema } from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { Model } from "mongoose";
+
+const EmailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+export interface IUser extends Document {
+    name: string;
+    email: string;
+    password: string;
+    avatar: {
+        public_id: string;
+        url: string;
+    };
+    role: string;
+    isVerified: boolean;
+    courses: Array<{
+        courseId: String;
+    }>;
+    comparePassword: (enteredPassword: string) => Promise<boolean>;
+
+};
+
+const userSchema: Schema<IUser> = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "Please enter your name"],
+            maxLength: [30, "Name cannot exceed 30 characters"],
+            minLength: [4, "Name should have more than 4 characters"],
+        },
+        email: {
+            type: String,
+            required: [true, "Please enter your email"],
+            unique: true,
+            validate: {
+                validator: function (email: string) {
+                    return EmailRegex.test(email);
+                },
+                message: "Please enter a valid email address",
+            },
+        },
+        password: {
+            type: String,
+            required: [true, "Please enter your password"],
+            minLength: [8, "Password should be greater than 8 characters"],
+            select: false,
+        },
+        avatar: {
+            public_id: {
+                type: String,
+                required: true,
+            },
+        },
+        role: {
+            type: String,
+            default: "user",
+        },
+        isVerified: {
+            type: Boolean,
+            default: false,
+        },
+        courses: [
+            {
+                courseId: {
+                    type: String,
+
+                },
+            },
+        ],
+    }, { timestamps: true });
+
+//Hashing password before saving user
+userSchema.pre<IUser>("save", async function (this: IUser, next: (err?: Error) => void) {
+    if (!this.isModified("password")) {
+        return next();
+    }
+
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+//Compare user password
+userSchema.methods.comparePassword = async function (enteredPassword: string) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const User: Model<IUser> = mongoose.model<IUser>("User", userSchema);
+
+export default User; 
